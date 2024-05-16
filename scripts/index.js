f6acb12539165114b016300a1e6c1a8e563da122
g-dilek <grace.persion@gmail.com>
2024-05-16 13:49:13 -0500

    Add JS card iterating functionality

--- scripts/index.js
+++ scripts/index.js
@@ -35,6 +35,9 @@ const profileSubtitle = document.querySelector("#profile-subtitle");
 const profileEditTitle = document.querySelector("#profile-edit-title");
 const profileEditSubtitle = document.querySelector("#profile-edit-subtitle");
 const profileEditForm = document.querySelector("#profile-edit-form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector("#card-list");
 
 // ! FUNCTIONS
 
@@ -42,8 +45,22 @@ function closePopUp() {
   profileEditModal.classList.remove("modal__opened");
 }
 
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector("#card-image");
+  const cardTitleEl = cardElement.querySelector("#card-title");
+  const cardSubtitleEl = cardElement.querySelector("#card-subtitle");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardTitleEl.textContent;
+
+  return cardElement;
+}
+
 // ! EVENT HANDLERS
 
+// Prevents page from refreshing when 'save' is clicked
+// Adds text entered in modal to profile
 function handleProfileEditSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = profileEditTitle.value;
@@ -67,4 +84,11 @@ profileEditButton.addEventListener("click", () => {
 // When modal close button clicked, close modal
 profileCloseButton.addEventListener("click", closePopUp);
 
+// When 'save' clicked, prevent page refreshing and
+// add text entered to profile
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
+});
