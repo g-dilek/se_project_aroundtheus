72a73efd4c002876355a293b821ba5aec2f803b1
g-dilek <grace.persion@gmail.com>
2024-06-16 10:03:01 -0500

    Changes to JS validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -8,17 +8,50 @@ const config = {
   errorClass: "modal__error_visible",
 };
 
+function showInputError() {}
+
+function checkInputValidity() {}
+
+// listen for event of typing in input boxes
 function setEventListeners(formEl, config) {
+  const errorMessage = document.querySelectorAll(".modal__error");
+  // make new variable that is the inputSelector property
+  // of object config
+  // same as const inputSelector = config.inputSelector
+  const { inputSelector } = config;
   // shorthand for modal form inputs
-  const inputEls = Array.from(formEl.querySelectorAll(config.inputSelector));
+  // makes 1 group of inputs listed per modal (name, desc, etc)
+  const inputEls = Array.from(formEl.querySelectorAll(inputSelector));
+  inputEls.forEach((inputEl) => {
+    // listens for new text in input boxes
+    // "change" occurs when user clicks out of textbox (more common)
+    inputEl.addEventListener("input", (evt) => {
+      // use browser's API to check event of inputting text
+      // evt.target === inputEl
+      // console.dir lets us view all properties, including validity->valid boolean
+      // this is the automatic message from HTML that gives validity check and message:
+      if (inputEl.validity.valid) {
+        inputEl.classList.remove("modal__input_type_error");
+        errorMessage.forEach((element) => {
+          element.classList.remove("modal__error_visible");
+        });
+      } else {
+        inputEl.classList.add("modal__input_type_error");
+        errorMessage.forEach((element) => {
+          element.classList.add("modal__error_visible");
+        });
+      }
+    });
+  });
 }
 
 function enableValidation(config) {
+  const { formSelector } = config;
   // grabs all HTML 'form' tags
   // NodeList: [form#profile-edit-form.modal__form, form#profile-add-card-form.modal__form]
   // NodeList: not an array, a list of node items
   // Certain methods don't work on NodeLists
-  const formEls = Array.from(document.querySelectorAll(config.formSelector));
+  const formEls = Array.from(document.querySelectorAll(formSelector));
   console.log(
     // iterate through both types of modal i have
     formEls.forEach((formEl) => {
